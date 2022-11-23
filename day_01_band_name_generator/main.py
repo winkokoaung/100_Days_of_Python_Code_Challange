072307400d82974160979e9d6fbe9dc6dc910ab0
Win Ko Ko Aung <winkokoaung@outlook.com>
2022-11-22 21:19:06 -0800

    Add files via upload

---
+++ day_01_band_name_generator/main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print ("Welcome to the band name generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grown up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your brand name could Be" + city + " " + pet)
